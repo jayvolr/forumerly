b2102d1c0531249f1312778ecbe7d6b930730bba
Jon Love <jonlov4@gmail.com>
2017-04-20 18:11:38 -0400

    test

--- db.js
+++ db.js
@@ -1,9 +1,9 @@
 const MongoClient = require('mongodb').MongoClient
 
-MongoClient.connect('mongodb://localhost:27017/forumerly', (err, connection) => {
-  if (err) {console.log(err)}
+MongoClient.connect('mongodb://107.170.32.124:27017/forumerly', (err, connection) => {
+  if (err) {console.warn('error from db.js'); console.log(err)}
   else {
     module.exports.db = connection
-    console.log('Connected to mongodb successfully.');
+    console.log('Connected to mongodb successfully!')
   }
 })
